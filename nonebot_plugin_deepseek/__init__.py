828e21ee0a79f2a1a8209e3668529606e9ed73aa
KomoriDev <mute231010@gmail.com>
2025-02-01 20:01:42 +0800

    :bookmark: v0.1.5

--- nonebot_plugin_deepseek/__init__.py
+++ nonebot_plugin_deepseek/__init__.py
@@ -57,7 +57,7 @@ __plugin_meta__ = PluginMetadata(
     extra={
         "unique_name": "DeepSeek",
         "author": "Komorebi <mute231010@gmail.com>",
-        "version": "0.1.4",
+        "version": "0.1.5",
     },
 )
 
